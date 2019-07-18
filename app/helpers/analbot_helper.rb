c32048d90039a777dbc6fb985a5268784d8cb7c3
yaaasuuu <hamada.yasuhiro584@mail.kyutech.jp>
2019-07-18 20:36:48 +0900

    heroku commit

---
+++ app/helpers/analbot_helper.rb
@@ -0,0 +1,2 @@
+module AnalbotHelper
+end
---
+++ test/controllers/analbot_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AnalbotControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
