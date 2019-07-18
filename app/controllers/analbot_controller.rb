24c3cc54bf6cb59b75421307c1dae0aca48ab8d7
yaaasuuu <hamada.yasuhiro584@mail.kyutech.jp>
2019-07-18 21:02:40 +0900

    heroku commit 3

--- app/controllers/analbot_controller.rb
+++ app/controllers/analbot_controller.rb
@@ -15,52 +15,27 @@ class AnalbotController < ApplicationController
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-      head :bad_request
+        error 400 do 'Bad Request' end
     end
 
     events = client.parse_events_from(body)
-
     events.each { |event|
-      case event
-      when Line::Bot::Event::Message
+        case event
+        when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          # LINEから送られてきたメッセージが「アンケート」と一致するかチェック
-          if event.message['text'].eql?('アンケート')
-            # private内のtemplateメソッドを呼び出します。
-            client.reply_message(event['replyToken'], template)
-          end
+            message = {
+            type: 'text',
+            text: event.message['text']
+            }
+            client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+            response = client.get_message_content(event.message['id'])
+            tf = Tempfile.open("content")
+            tf.write(response.body)
         end
-      end
+        end
     }
-
     head :ok
-  end
-
-  private
-
-  def template
-    {
-      "type": "template",
-      "altText": "this is a confirm template",
-      "template": {
-          "type": "confirm",
-          "text": "今日のもくもく会は楽しいですか？",
-          "actions": [
-              {
-                "type": "message",
-                # Botから送られてきたメッセージに表示される文字列です。
-                "label": "楽しい",
-                # ボタンを押した時にBotに送られる文字列です。
-                "text": "楽しい"
-              },
-              {
-                "type": "message",
-                "label": "楽しくない",
-                "text": "楽しくない"
-              }
-          ]
-      }
-    }
   end
 end
