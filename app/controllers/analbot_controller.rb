8c52cccbfbbbcc527889e01a1fb9da4669655286
yaaasuuu <hamada.yasuhiro584@mail.kyutech.jp>
2019-08-09 03:10:32 +0900

    test v2

--- app/controllers/analbot_controller.rb
+++ app/controllers/analbot_controller.rb
@@ -30,7 +30,8 @@ class AnalbotController < ApplicationController
             when Line::Bot::Event::MessageType::Location
               user_lat = event.message['latitude'] # 緯度
               user_long = event.message['longitude'] # 経度
-              
+              id = cal_address(user_long, user_lat)
+              ms = "#{id}"
 
             when Line::Bot::Event::MessageType::Text
               case event.message['text']
@@ -81,9 +82,48 @@ class AnalbotController < ApplicationController
   end
 
   def cal_address(user_long, user_lat)
-    locations = Location.pluck('id','lat','long')
+    locations = Location.pluck('id', 'lat', 'long')
     locations.each do |loc|
-      puts loc
+      id = loc[0]
+      lat = loc[1]
+      long = loc[2]
+
+      min = 100
+      d = distance(user_lat, user_long, lat, long).round(6)
+      if d < min
+        min = d
+        min_id = id
+      end
     end
+    return id
+  end
+
+  def distance(lat1, lng1, lat2, lng2)
+    # ラジアン単位に変換
+    x1 = lat1.to_f * Math::PI / 180
+    y1 = lng1.to_f * Math::PI / 180
+    x2 = lat2.to_f * Math::PI / 180
+    y2 = lng2.to_f * Math::PI / 180
+    
+    # 地球の半径 (km)
+    radius = 6378.137
+    
+    # 差の絶対値
+    diff_y = (y1 - y2).abs
+    
+    calc1 = Math.cos(x2) * Math.sin(diff_y)
+    calc2 = Math.cos(x1) * Math.sin(x2) - Math.sin(x1) * Math.cos(x2) * Math.cos(diff_y)
+    
+    # 分子
+    numerator = Math.sqrt(calc1 ** 2 + calc2 ** 2)
+    
+    # 分母
+    denominator = Math.sin(x1) * Math.sin(x2) + Math.cos(x1) * Math.cos(x2) * Math.cos(diff_y)
+    
+    # 弧度
+    degree = Math.atan2(numerator, denominator)
+    
+    # 大円距離 (km)
+    return degree * radius
   end
 end
